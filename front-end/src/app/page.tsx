61d35bf1610b6b1b16dd54ae3cc614e083030ba6
samuel_ndjouli <samuelndj.997@gmail.com>
2024-01-09 13:47:33 +0100

    imported ChooseApps component

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,7 +1,12 @@
+import ChooseApps from './components/layout/ChooseApps'
 import Header from './components/layout/Header'
 
 export default function Home() {
   return (
-    <Header />
+    <>
+      <Header />
+      <ChooseApps />
+    </>
+
   )
 }
