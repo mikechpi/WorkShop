969f2ac924b959e3a9b7393ffbb2f86f007ddfa0
samuel_ndjouli <samuelndj.997@gmail.com>
2024-01-10 11:43:38 +0100

    removed the padding top

--- front-end/src/app/components/layout/ChooseApps.tsx
+++ front-end/src/app/components/layout/ChooseApps.tsx
@@ -7,7 +7,7 @@ import { Button } from '@/components/ui/button'
 const ChooseApps = () => {
   return (
     <SectionComponent>
-        <div className='flex flex-col space-y-4 justify-center items-center mt-8'>
+        <div className='flex flex-col space-y-4 justify-center items-center'>
           <h2 className='lg:text-3xl text-gray-800 font-bold text-center'>Choose your apps</h2>
           <p className='text-center text-gray-600 lg:max-w-4xl'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec aliquet massa, sed dapibus mauris. Donec aliquet, nibh sed ultrices interdum, nunc nibh ultricies dolor, quis aliquam nunc nisl quis nunc. </p>
         </div>
