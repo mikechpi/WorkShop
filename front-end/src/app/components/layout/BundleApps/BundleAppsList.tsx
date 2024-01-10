32423dbfe2d3a3d222e13896355970e80e72fbf1
samuel_ndjouli <samuelndj.997@gmail.com>
2024-01-10 12:15:23 +0100

    made some modifications with the colors

--- front-end/src/app/components/layout/BundleApps/BundleAppsList.tsx
+++ front-end/src/app/components/layout/BundleApps/BundleAppsList.tsx
@@ -10,14 +10,14 @@ interface BundleAppsListProps {
 
 const BundleAppsList = ({app}: BundleAppsListProps) => {
   return (
-    <div className="px-4 py-3 rounded-lg shadow transition-all duration-300 hover:bg-gray-100">
+    <div className="px-4 py-3 rounded-lg shadow transition-all border-slate-300 duration-300 hover:bg-gray-100">
       <div className="flex justify-between gap-4">
         <div className="shrink-0">
           <Image src={app.image} className="h-12 w-12 rounded-lg object-cover shadow-sm" width={48} height={48} alt="illustration" />
         </div>
         <div className='w-full'>
           <h4 className='lg:text-md text-gray-800 font-bold'>{app.name}</h4>
-          <span className="rounded-full px-2.5 py-0.5 text-[10px]  border text-emerald-600 border-emerald-400">
+          <span className="rounded-full px-2.5 py-0.5 font-semibold text-[10px] text-[#3B82F6] bg-[#3B82F6] bg-opacity-10 border border-[#3B82F6]/75">
             Lecture
           </span>
         </div>
