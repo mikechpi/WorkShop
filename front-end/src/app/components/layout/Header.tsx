40a842c1287071c75b09585cc16b3ce5b76a7f96
Aurel Yahouedeou <admofficiel@gmail.com>
2024-01-10 10:20:38 +0100

    added some codes in the header

--- front-end/src/app/components/layout/Header.tsx
+++ front-end/src/app/components/layout/Header.tsx
@@ -1,43 +1,21 @@
 import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header style={headerStyle}>
-      <div style={logoContainer}>
-        <img src={"./img/Logo.webp"} alt="Logo YunoHost 4Everyone" style={logoStyles} />
+    <header className="top-0 left-0 right-0 bg-white px-4 py-2 md:p-4 lg:px-12 flex justify-between items-center shadow-lg"> {/* Styles de la Nav-bar*/}
+      <div className="mr-auto"> {/* Le Logo */}
+        <Image src="/img/Logo.webp" alt="Logo YunoHost 4Everyone" width={60} height={30} className="max-w-xxs" />
       </div>
-      <div style={menuContainer}>
-        <div style={menuItem}>Bundles</div>
-        <div style={menuItem}>Applications</div>
-        <div style={menuItem}>Diagnostic</div>
+      <div className="hidden md:flex items-center space-x-8"> {/* Les items */}
+        <Link href="https://md.yeswiki.net/s/3zWb4SQR8" target="_blank" rel="noopener noreferrer" className="cursor-pointer font-semibold text-blue-500 hover:text-pink-500 transition duration-700 ease-in-out">Bundles</Link>
+        <Link href="https://apps.yunohost.org/catalog" target="_blank" rel="noopener noreferrer" className="cursor-pointer font-semibold text-blue-500 hover:text-pink-500 transition duration-700 ease-in-out">Applications</Link>
+        <Link href="https://yunohost.org/fr/troubleshooting" target="_blank" rel="noopener noreferrer" className="cursor-pointer font-semibold text-blue-500 hover:text-pink-500 transition duration-700 ease-in-out">Diagnostic</Link>
       </div>
+
     </header>
   );
-};
-
-const headerStyle = {
-  display: 'flex',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-  padding: '10px',
-  background: '#fff',
-  color: '#3B82F6',
-};
-
-const logoContainer = {
-  // Add styles for the logo container if needed
-};
-
-const logoStyles = {
-  maxWidth: '100px', // Ajustez la largeur du logo selon vos besoins
-};
-
-const menuContainer = {
-  display: 'flex',
-};
-
-const menuItem = {
-  margin: '0 10px',
 };
 
 export default Header;
