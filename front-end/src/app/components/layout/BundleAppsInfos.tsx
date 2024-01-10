735f77af9958e2670e9fa4849353b64b802aa9a5
samuel_ndjouli <samuelndj.997@gmail.com>
2024-01-10 11:42:15 +0100

    changed the architecture of the code, removed the button imported from shadcn

--- front-end/src/app/components/layout/BundleAppsInfos.tsx
+++ front-end/src/app/components/layout/BundleAppsInfos.tsx
@@ -1,26 +1,36 @@
-import { Button } from '@/components/ui/button'
+import Image from 'next/image'
 import React from 'react'
 
 interface BundleAppsInfosProps {
   title: string,
   apps: {
-     name: string
+     name: string,
+     image: string,
   }[],
 }
 
 const BundleAppsInfos = ({title, apps}: BundleAppsInfosProps) => {
   return (
     <div className='flex flex-col mt-8 justify-center items-center'>
-        <Button variant="ghost" className="lg:text-2xl text-gray-800 font-bold">{title}</Button>
-        {/* <h3 className='lg:text-2xl text-gray-800 font-bold'>Post-it / Kanban / Suivi de chantier / Tempête de cerveaux</h3> */}
-        <hr className='w-1/6 border-gray-300  mt-4' />
-        <div className='flex space-x-4 mt-6 justify-start items-center'>
+        <h3 className='lg:text-2xl text-gray-800 font-bold'>{title}</h3>
+        <hr className='w-1/6 border-gray-300 mt-4 mb-8' />
+        <div className="grid grid-cols-1 lg:grid-cols-3 lg:gap-8">
           {
             apps.map((app, index) => {
               return (
-                  <div className='bg-slate-100 rounded-lg px-4 py-2' key={index}>
-                    <p className='text-gray-800 font-bold text-center'>{app.name}</p>
+                <div className="px-4 py-3 rounded-lg shadow transition-all duration-300 hover:bg-gray-100" key={index}>
+                  <div className="flex justify-between gap-4">
+                    <div className="shrink-0">
+                      <Image src={app.image} className="h-12 w-12 rounded-lg object-cover shadow-sm" width={48} height={48} alt="illustration" />
+                    </div>
+                    <div className='w-full'>
+                      <h4 className='lg:text-md text-gray-800 font-bold'>{app.name}</h4>
+                      <span className="rounded-full px-2.5 py-0.5 text-[10px]  border text-emerald-600 border-emerald-400">
+                        Lecture
+                      </span>
+                    </div>
                   </div>
+                </div>
               )
             })
           }
