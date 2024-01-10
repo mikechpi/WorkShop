a96d106279e9cf2b3d638c04f72c9956fafb013a
samuel_ndjouli <samuelndj.997@gmail.com>
2024-01-10 12:02:39 +0100

    Created a new component to display the bundle's app

---
+++ front-end/src/app/components/layout/BundleApps/BundleAppsDisplay.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image'
+import React from 'react'
+import BundleAppsList from './BundleAppsList'
+
+interface BundleAppsDisplayProps {
+  title: string,
+  apps: {
+     name: string,
+     image: string,
+  }[],
+}
+
+const BundleAppsDisplay = ({title, apps}: BundleAppsDisplayProps) => {
+  return (
+    <div className='flex flex-col mt-8 justify-center items-center'>
+        <h3 className='lg:text-2xl text-gray-800 font-bold'>{title}</h3>
+        <hr className='w-1/6 border-gray-300 mt-4 mb-8' />
+        <div className="grid grid-cols-1 lg:grid-cols-3 lg:gap-8">
+          {
+            apps.map((app, index) => {
+              return (
+                <BundleAppsList key={index} app={app} />
+              )
+            })
+          }
+        </div>
+    </div>
+  )
+}
+
+export default BundleAppsDisplay
