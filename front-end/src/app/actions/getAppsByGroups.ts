9267eb4114cd9c4ca57f85cf7231dd21533c2756
samuel_ndjouli <samuelndj.997@gmail.com>
2024-01-11 15:31:34 +0100

    created an interface App

--- front-end/src/app/actions/getAppsByGroups.ts
+++ front-end/src/app/actions/getAppsByGroups.ts
@@ -1,10 +1,17 @@
+interface App {
+    name: string;
+    logoUrl: string;
+    categorie: string;
+    groups: string[];
+  }
+  
 export default async function getAppsByGroup(group: string) {
     const response = await fetch('http://localhost:8000/apps');
     const apps = await response.json();
   
     // Filter apps based on the specified group
     const filteredApps = group
-      ? apps.filter(app => app.groups.includes(group))
+      ? (apps as App[]).filter(app => app.groups.includes(group))
       : apps;
   
     return filteredApps;
