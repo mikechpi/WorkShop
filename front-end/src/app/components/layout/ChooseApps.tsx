339415262c1a33126e2f82eea54a11c065ed027f
samuel_ndjouli <samuelndj.997@gmail.com>
2024-01-11 15:16:57 +0100

    removed use client, imported 3 server components, and removed the map loop

--- front-end/src/app/components/layout/ChooseApps.tsx
+++ front-end/src/app/components/layout/ChooseApps.tsx
@@ -1,27 +1,24 @@
-import React, { useEffect, useState } from 'react'
 import SectionComponent from '../SectionComponent'
-import { BundleAppsData } from '@/app/utils/constants/BundleAppsInfos'
 import { Button } from '@/components/ui/button'
 import BundleAppsDisplay from './bundleapps/BundleAppsDisplay'
-import getApps from '@/app/actions/getApps'
+import getAppsByGroup from '@/app/actions/getAppsByGroups'
 
 
 const ChooseApps = async () => {
-  const appsList = await getApps()
+
+  const postItApps = await getAppsByGroup('post-it');
+  const coEcritureApps = await getAppsByGroup('co-écriture');
+  const biblioApps = await getAppsByGroup('biblio');
+
   return (
     <SectionComponent>
         <div className='flex flex-col space-y-4 justify-center items-center'>
           <h2 className='lg:text-3xl text-gray-800 font-bold text-center'>Choose your apps</h2>
           <p className='text-center text-gray-600 lg:max-w-4xl'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec aliquet massa, sed dapibus mauris. Donec aliquet, nibh sed ultrices interdum, nunc nibh ultricies dolor, quis aliquam nunc nisl quis nunc. </p>
         </div>
-        {
-          BundleAppsData.map((infos, index) => {
-              const { title, apps } = infos;
-              return (
-                  <BundleAppsDisplay key={index} title={title} apps={ appsList } />
-              )
-          })
-        }
+        <BundleAppsDisplay title="Post-it" appsList={ postItApps } />
+        <BundleAppsDisplay title="Co-écriture" appsList={ coEcritureApps } />
+        <BundleAppsDisplay title="Bibliothèque" appsList={ biblioApps } />
         <div className="text-center my-6">
           <Button className ="mt-4 bg-gradient-to-r from-[#FF3399] to-[#3B82F6] transition-opacity duration-500 opacity-70 hover:opacity-100">Enregistrer vos modifications</Button>
         </div>
