bcc9df3cd5d72a83fe83d7c03659d4074bb55cba
samuel_ndjouli <samuelndj.997@gmail.com>
2024-01-12 11:42:54 +0100

    made some modifications in Loader

--- front-end/src/app/components/layout/loader/Loader.tsx
+++ front-end/src/app/components/layout/loader/Loader.tsx
@@ -1,9 +1,17 @@
-import { Progress } from '@radix-ui/react-progress'
-import React from 'react'
+"use client"
+import { Progress } from '@/components/ui/progress';
+import React, { useEffect, useState } from 'react'
 
 const Loader = () => {
+    const [progress, setProgress] = useState<number>(13);
+
+    useEffect(() => {
+        const timer = setTimeout(() => setProgress(66), 500)
+        return () => clearTimeout(timer)
+      }, [])
+
   return (
-    <Progress value={33} />
+    <Progress value={progress} className="w-[90%] text-center my-16 mx-14 bg-emerald-200" />
   )
 }
 
